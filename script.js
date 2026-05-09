5830b8f06eefdc679fa387881cfe9ad75ae75363
kiritosama659-stack <kiritosama659@gmail.com>
2026-05-09 15:55:46 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function scrollToSection(){
+
+  document.getElementById("context")
+  .scrollIntoView({
+    behavior:"smooth"
+  });
+
+}
+
+
+const fadeElements = document.querySelectorAll('.fade-in');
+
+window.addEventListener('scroll', () => {
+
+  fadeElements.forEach((element) => {
+
+    const position = element.getBoundingClientRect().top;
+
+    const screenPosition = window.innerHeight / 1.2;
+
+    if(position < screenPosition){
+      element.classList.add('show');
+    }
+
+  });
+
+});
